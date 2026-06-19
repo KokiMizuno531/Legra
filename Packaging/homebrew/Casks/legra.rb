af246b567acbf66843b51ac5ff93975a5b0dbe82
Koki Mizuno <k_mizuno@st.phys.nagoya-u.ac.jp>
2026-06-19 19:30:13 +0900

    Use canonical Legra release URL in cask

--- Packaging/homebrew/Casks/legra.rb
+++ Packaging/homebrew/Casks/legra.rb
@@ -2,10 +2,10 @@ cask "legra" do
   version "0.1.0"
   sha256 "fec90b57bc454f76872a68004f205750f948f48130d40276519845d8d0bf2e8d"
 
-  url "https://github.com/KokiMizuno531/legra/releases/download/v#{version}/Legra_#{version}_aarch64.app.zip"
+  url "https://github.com/KokiMizuno531/Legra/releases/download/v#{version}/Legra_#{version}_aarch64.app.zip"
   name "Legra"
   desc "Local-first paper, PDF, note, and BibTeX manager"
-  homepage "https://github.com/KokiMizuno531/legra"
+  homepage "https://github.com/KokiMizuno531/Legra"
 
   depends_on macos: ">= :big_sur"
 
